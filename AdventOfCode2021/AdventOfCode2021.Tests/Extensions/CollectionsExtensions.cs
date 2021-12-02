4ee46af20acb6247a411b57a2bd027bf19c4aa93
bob h <mail@bob.codes>
2021-12-02 01:30:02 +0000

    some improvements

--- AdventOfCode2021/AdventOfCode2021.Tests/Extensions/CollectionsExtensions.cs
+++ AdventOfCode2021/AdventOfCode2021.Tests/Extensions/CollectionsExtensions.cs
@@ -5,7 +5,7 @@ namespace System.Collections.Generic;
 
 public static class CollectionsExtensions
 {
-	private static ChangeTypes Compare(IComparable left, IComparable right)
+	private static ChangeTypes Compare(IComparable left, object? right)
 		=> left.CompareTo(right) switch
 		{
 			< 0 => ChangeTypes.Increased,
@@ -16,32 +16,23 @@ public static class CollectionsExtensions
 	public static IEnumerable<ChangeTypes> Increasings<T>(this IEnumerable<T> items)
 		where T : IComparable
 	{
-		using var enumerator = items.GetEnumerator();
+		var pairs = items.ToWindows(size: 2);
 
-		enumerator.MoveNext();
-		var prev = enumerator.Current;
-
-		while (enumerator.MoveNext())
+		foreach (var (first, second) in pairs)
 		{
-			var curr = enumerator.Current;
-			yield return Compare(prev, curr);
-			prev = curr;
+			yield return Compare(first!, second);
 		}
 	}
 
 	public async static IAsyncEnumerable<ChangeTypes> IncreasingsAsync<T>(this IAsyncEnumerable<T> items, CancellationToken? cancellationToken = default)
 		where T : IComparable
 	{
-		await using var enumerator = items.GetAsyncEnumerator(cancellationToken ?? CancellationToken.None);
+		var pairs = items.ToWindowsAsync(size: 2, cancellationToken);
 
-		await enumerator.MoveNextAsync(cancellationToken ?? CancellationToken.None);
-		var prev = enumerator.Current;
-
-		while (await enumerator.MoveNextAsync(cancellationToken ?? CancellationToken.None))
+		await foreach (var (first, second) in pairs)
 		{
-			var curr = enumerator.Current;
-			yield return Compare(prev, curr);
-			prev = curr;
+			if (cancellationToken?.IsCancellationRequested == true) throw new OperationCanceledException(cancellationToken!.Value);
+			yield return Compare(first!, second);
 		}
 	}
 
@@ -56,26 +47,37 @@ public static class CollectionsExtensions
 
 			if (count == size)
 			{
-				yield return queue.ToList();
+				yield return queue.ToArray();
 				queue.TryDequeue(out var _);
 			}
 		}
 	}
 
-	public async static IAsyncEnumerable<IReadOnlyCollection<T>> ToWindowsAsync<T>(this IAsyncEnumerable<T> items, int size = 3)
+	public async static IAsyncEnumerable<IReadOnlyCollection<T>> ToWindowsAsync<T>(
+		this IAsyncEnumerable<T> items,
+		int size = 3,
+		CancellationToken? cancellationToken = default)
 	{
 		var queue = new ConcurrentQueue<T>();
 
 		await foreach (var item in items)
 		{
+			if (cancellationToken?.IsCancellationRequested == true) throw new OperationCanceledException(cancellationToken!.Value);
 			queue.Enqueue(item);
 			var count = queue.Count;
 
 			if (count == size)
 			{
-				yield return queue.ToList();
+				yield return queue.ToArray();
 				queue.TryDequeue(out var _);
 			}
 		}
+	}
+
+	public static void Deconstruct<T>(this IEnumerable<T> items, out T? first, out T? second)
+	{
+		using var enumerator = items.GetEnumerator();
+		first = enumerator.MoveNext() ? enumerator.Current : default;
+		second = enumerator.MoveNext() ? enumerator.Current : default;
 	}
 }
